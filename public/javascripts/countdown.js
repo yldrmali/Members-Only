aae48cc1386322cf04550f3678d9b3239874f011
yldrmali <yldrmmmali.2@gmail.com>
2020-06-09 12:30:39 +0300

    change location reference

--- public/javascripts/countdown.js
+++ public/javascripts/countdown.js
@@ -3,7 +3,8 @@ var downloadTimer = setInterval(function(){
     if(timeleft <= 0){
       clearInterval(downloadTimer);
       document.getElementById("countdown").innerHTML = "Sorry:)), you are redirected";
-      window.location.href = "http://localhost:3000/main"
+      //change location href to https://localhost:3000/main to use domain locally
+      window.location.href = "https://sheltered-dusk-63801.herokuapp.com/main"
     } else {
     document.getElementById("countdown").innerHTML = `<strong>${timeleft}</strong>`+' second remains';
     }
